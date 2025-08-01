83f94c5a2c9e12894658250b58d1b06a8219e5bf
D A R K <abdu.ewian@gmail.com>
2025-08-01 21:23:36 +0300

    Add files via upload

---
+++ View/AddEditPhoneLineWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PhoneLinesApp.UI.Views
+{
+    public partial class AddEditPhoneLineWindow : Window
+    {
+        public AddEditPhoneLineWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
